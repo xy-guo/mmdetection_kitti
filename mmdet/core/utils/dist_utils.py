efaf9d67ea7180c7f809abcfa2d184c063c1b36c
Xiaoyang Guo <xiaoyang.guo1995@gmail.com>
2020-11-27 01:16:49 +0800

    add atss

--- mmdet/core/utils/dist_utils.py
+++ mmdet/core/utils/dist_utils.py
@@ -65,5 +65,6 @@ def reduce_mean(tensor):
     if not (dist.is_available() and dist.is_initialized()):
         return tensor
     tensor = tensor.clone()
-    dist.all_reduce(tensor.div_(dist.get_world_size()), op=dist.ReduceOp.SUM)
+    dist.all_reduce(tensor.floor_divide(float(dist.get_world_size())),
+                    op=dist.ReduceOp.SUM)
     return tensor
