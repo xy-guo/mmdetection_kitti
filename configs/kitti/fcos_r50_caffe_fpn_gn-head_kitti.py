ceb3fc8aefa629f43f7f1a4efc1064a55849b59b
Xiaoyang Guo <xiaoyang.guo1995@gmail.com>
2020-11-27 02:43:49 +0800

    potentially fix fcos bug; make warmup config consistent

--- configs/kitti/fcos_r50_caffe_fpn_gn-head_kitti.py
+++ configs/kitti/fcos_r50_caffe_fpn_gn-head_kitti.py
@@ -2,9 +2,9 @@ _base_ = [
     '../_base_/datasets/kitti_mono.py',
     '../_base_/default_runtime.py'
 ]
-# VFNet Model
+# FCOS model
 model = dict(
-    type='VFNet',
+    type='FCOS',
     pretrained=None,
     backbone=dict(
         type='ResNet',
@@ -14,7 +14,7 @@ model = dict(
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
-        style='pytorch'),
+        style='caffe'),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -25,29 +25,29 @@ model = dict(
         num_outs=5,
         relu_before_extra_convs=True),
     bbox_head=dict(
-        type='VFNetHead',
+        type='FCOSHead',
         num_classes=3,
         in_channels=256,
-        stacked_convs=3,
+        stacked_convs=4,
         feat_channels=256,
         strides=[8, 16, 32, 64, 128],
-        center_sampling=False,
-        dcn_on_last_conv=False,
-        use_atss=True,
-        use_vfl=True,
         loss_cls=dict(
-            type='VarifocalLoss',
+            type='FocalLoss',
             use_sigmoid=True,
-            alpha=0.75,
             gamma=2.0,
-            iou_weighted=True,
+            alpha=0.25,
             loss_weight=1.0),
-        loss_bbox=dict(type='GIoULoss', loss_weight=1.5),
-        loss_bbox_refine=dict(type='GIoULoss', loss_weight=2.0)))
-
-# training and testing settings for VFNet
+        loss_bbox=dict(type='IoULoss', loss_weight=1.0),
+        loss_centerness=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)))
+# training and testing settings for FCOS
 train_cfg = dict(
-    assigner=dict(type='ATSSAssigner', topk=9),
+    assigner=dict(
+        type='MaxIoUAssigner',
+        pos_iou_thr=0.5,
+        neg_iou_thr=0.4,
+        min_pos_iou=0,
+        ignore_iof_thr=-1),
     allowed_border=-1,
     pos_weight=-1,
     debug=False)
@@ -55,24 +55,22 @@ test_cfg = dict(
     nms_pre=1000,
     min_bbox_size=0,
     score_thr=0.05,
-    nms=dict(type='nms', iou_threshold=0.6),
+    nms=dict(type='nms', iou_threshold=0.5),
     max_per_img=100)
+img_norm_cfg = dict(
+    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001,
                  paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
-optimizer_config = dict(grad_clip=None)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
-    warmup_ratio=0.1,
+    warmup_ratio=0.001,
     step=[16, 22])
 total_epochs = 24
 log_config = dict(interval=10)
-# For better, more stable performance initialize from COCO: 41.6AP
-load_from = 'https://openmmlab.oss-cn-hangzhou.aliyuncs.com/mmdetection/v2.0/vfnet/vfnet_r50_fpn_1x_coco/vfnet_r50_fpn_1x_coco_20201027-38db6f58.pth'
-# 44.8AP
-# load_from = 'https://openmmlab.oss-cn-hangzhou.aliyuncs.com/mmdetection/v2.0/vfnet/vfnet_r50_fpn_mstrain_2x_coco/vfnet_r50_fpn_mstrain_2x_coco_20201027-7cc75bd2.pth'
-# 48.0AP
-# load_from = 'https://openmmlab.oss-cn-hangzhou.aliyuncs.com/mmdetection/v2.0/vfnet/vfnet_r50_fpn_mdconv_c3-c5_mstrain_2x_coco/vfnet_r50_fpn_mdconv_c3-c5_mstrain_2x_coco_20201027pth-6879c318.pth'
+# For better, more stable performance initialize from COCO: 36.6AP
+load_from = 'http://download.openmmlab.com/mmdetection/v2.0/fcos/fcos_r50_caffe_fpn_gn-head_4x4_1x_coco/fcos_r50_caffe_fpn_gn_1x_4gpu_20200218-7831950c.pth'
