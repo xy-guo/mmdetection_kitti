3e61c78be8fa1eb824f2739c7a2db6f40236e7ed
Xiaoyang Guo <xyguo@link.cuhk.edu.hk>
2020-12-16 16:44:40 +0800

    add cityscapes pretraining mode

---
+++ configs/kitti_psmlike34/pretraincs_atss_psmlike34_nofpn_1x_kitti.py
@@ -0,0 +1,80 @@
+# TODO: not working
+_base_ = [
+    '../_base_/datasets/cityscapes_detection.py',
+    '../_base_/default_runtime.py'
+]
+# ATSS Model
+model = dict(
+    type='ATSS',
+    pretrained='torchvision://resnet34',
+    backbone=dict(
+        type='ResNet',
+        depth=34,
+        num_stages=4,
+        out_indices=(3,),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        with_max_pool=False,
+        strides=(1, 2, 1, 1),
+        dilations=(1, 1, 2, 4),
+        deep_stem=False,  # TODO: no pretrained model, cannot modify
+        block_with_final_relu=True,
+        base_channels=64),
+    neck=dict(
+        type='FPN',
+        in_channels=[512],
+        out_channels=256,  # TODO
+        start_level=0,
+        add_extra_convs='on_output',
+        num_outs=5),
+    bbox_head=dict(
+        type='ATSSHead',
+        num_classes=8,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            ratios=[1.0],
+            octave_base_scale=16,
+            scales_per_octave=1,
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[0.1, 0.1, 0.2, 0.2]),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
+        loss_centerness=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)))
+# training and testing settings for ATSS
+train_cfg = dict(
+    assigner=dict(type='ATSSAssigner', topk=9),
+    allowed_border=-1,
+    pos_weight=-1,
+    debug=False)
+test_cfg = dict(
+    nms_pre=1000,
+    min_bbox_size=0,
+    score_thr=0.05,
+    nms=dict(type='nms', iou_threshold=0.6),
+    max_per_img=100)
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[7])
+total_epochs = 8
+log_config = dict(interval=10)
