0417720243ece1e74c3eca8a8ff87997f5d03845
Xiaoyang Guo <xiaoyang.guo1995@gmail.com>
2020-12-17 16:27:19 +0800

    fix cs dataset config bug

--- configs/_base_/datasets/cityscapes_detection_small.py
+++ configs/_base_/datasets/cityscapes_detection_small.py
@@ -17,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 1024),
+        img_scale=(1024, 512),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
