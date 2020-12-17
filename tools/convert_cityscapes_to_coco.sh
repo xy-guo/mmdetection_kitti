b87ab0828d4542e6e76ac801672a507e97d23894
Xiaoyang Guo <xiaoyang.guo1995@gmail.com>
2020-12-17 14:08:15 +0800

    track new gt json for cityscapes [generated by scritps in mmdet]

--- tools/convert_cityscapes_to_coco.sh
+++ tools/convert_cityscapes_to_coco.sh
@@ -1,1 +1,2 @@
-python convert_cityscapes_to_coco.py  --dataset cityscapes_instance_only --datadir ~/data/cityscapes/ --outdir ../data/cityscapes/annotations/
+# python convert_cityscapes_to_coco.py  --dataset cityscapes_instance_only --datadir ~/data/cityscapes/ --outdir ../data/cityscapes/annotations/
+python tools/convert_datasets/cityscapes.py --img-dir leftImg8bit/ --gt-dir gtFine_trainvaltest/gtFine -o data/cityscapes/annotations/ data/cityscapes/ 
